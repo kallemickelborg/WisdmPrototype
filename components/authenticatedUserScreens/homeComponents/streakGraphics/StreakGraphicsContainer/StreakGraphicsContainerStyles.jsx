7fdcd3cd04c950461e4c953231095fa91f873435
Jonathan Lascano <jonathanlascano2393@gmail.com>
2023-03-23 17:22:05 -0700

    Finish streak tracking components

--- components/authenticatedUserScreens/homeComponents/streakGraphics/StreakGraphicsContainer/StreakGraphicsContainerStyles.jsx
+++ components/authenticatedUserScreens/homeComponents/streakGraphics/StreakGraphicsContainer/StreakGraphicsContainerStyles.jsx
@@ -1,24 +1,11 @@
 import { StyleSheet } from "react-native";
 
-import { radius, spacing, colors } from "../../../../../globalStyles";
-
 export const styles = StyleSheet.create({
-  button: {
-    borderRadius: radius.small,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: spacing.small,
-    paddingBottom: spacing.small,
-    width: '100%',
-    borderWidth: 2,
-    borderColor: 'transparent',
-    backgroundColor: colors.buttonPrimary,
-    flexDirection: 'row', 
-    flexWrap: 'nowrap'
-  },
-  selectedButton: {
-    backgroundColor: colors.secondary,
-    borderWidth: 2,
-    borderColor: colors.tertiary
+  streakContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    height: 10,
   }
 });
